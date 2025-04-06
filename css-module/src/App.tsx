ac8b3bf0f323b0fc60b3e81e7b8a17bdf1e65da9
Mayukh Basu <roxy.firebird@gmail.com>
2025-04-06 08:29:18 +0530

    basic changes

--- css-module/src/App.tsx
+++ css-module/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button';
 
-function App() {
+const App: React.FC = () => {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello React + TypeScript + CSS Modules</h1>
+      <Button text="Click Me" onClick={handleClick} />
     </div>
   );
-}
+};
 
 export default App;
